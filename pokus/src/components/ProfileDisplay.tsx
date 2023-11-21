cf13fd58b7cc18d94505fb0a9b0d7663e7593e6d
Srn4 <srnak02@gmail.com>
2023-11-21 23:37:09 +0100

    changed ./css

--- pokus/src/components/ProfileDisplay.tsx
+++ pokus/src/components/ProfileDisplay.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import 'ProfileDisplay.css'
+import './ProfileDisplay.css'
 
 const ProfileDisplay = () => {
   const navigate = useNavigate();
